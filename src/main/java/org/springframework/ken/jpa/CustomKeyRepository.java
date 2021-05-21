3d018e52e71f776bb5c735443d494c588f285749
kenkhwong <wongkh31@gmail.com>
2021-05-21 21:08:49 +0100

    Custom Key feature updates

--- src/main/java/org/springframework/ken/jpa/CustomKeyRepository.java
+++
@@ -1,6 +0,0 @@
-package org.springframework.ken.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomKeyRepository<K extends CustomKey<T>, T> extends JpaRepository<K,T> {
-}
